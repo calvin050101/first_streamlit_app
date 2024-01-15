0e6e0136981ed8297a3f313a3745d29af4d9149e
calvin050101 <148545431+calvin050101@users.noreply.github.com>
2024-01-15 15:24:38 +0800

    Update streamlit_app.py
    Added user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,11 +23,11 @@ streamlit.dataframe(fruits_to_show)
 
 # New Section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
 
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
-
-# write your own comment -what does the next line do? 
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
+streamlit.write('The user entered', fruit_choice);
+ 
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# write your own comment - what does this do?
 streamlit.dataframe(fruityvice_normalized)
